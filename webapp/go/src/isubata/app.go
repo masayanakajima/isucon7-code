cba1c5b8b28b6ba8ab6a9ea871197db264430fd5
masayanakajima <nakajima1812@gmail.com>
2021-05-25 23:27:51 +0900

    sleep削除

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -442,12 +442,7 @@ func fetchUnread(c echo.Context) error {
 		return c.NoContent(http.StatusForbidden)
 	}
 
-	time.Sleep(time.Second)
-
-	channels, err := queryChannels()
-	if err != nil {
-		return err
-	}
+	//time.Sleep(time.Second)
 
 	resp := []map[string]interface{}{}
 	rows, err := db.Query("select channel_id, count(*) from (select channel_id from message where ? < id ) group by channel_id",)
