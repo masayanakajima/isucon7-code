87481e6778492400965f5c5366131133d73f27c6
masayanakajima <nakajima1812@gmail.com>
2021-05-30 16:24:57 +0900

    getHistoryのN+1解消

--- webapp/go/src/isubata/main.go
+++ webapp/go/src/isubata/main.go
@@ -573,8 +573,8 @@ func getHistory(c echo.Context) error {
 	}
 
 	rev_mjson := make([]map[string]interface{}, 0)
-	for i := len(response) - 1; i >= 0; i-- {
-		rev_msjon = append(rev_mjson, msjon[i])
+	for i := len(msjon) - 1; i >= 0; i-- {
+		rev_mjson = append(rev_mjson, msjon[i])
 	}
 
 	channels := []ChannelInfo{}
